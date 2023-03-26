fbd838452a7fcd8c7eff6f29a3c1fb25d414bc5a
Victoria Holub <victoria_holub@epam.com>
2023-03-26 20:04:30 +0300

    lesson 2. file system module

--- modules.js
+++ modules.js
@@ -1,4 +1,4 @@
-const xyz = require("./people");
+global.xyz = require("./people");
 
 console.log(xyz);
 // console.log(people); // reference error
@@ -7,3 +7,15 @@ console.log(xyz.people, xyz.ages);
 const { people, ages } = require("./people");
 console.log("----------");
 console.log(people, ages);
+
+// setTimeout(() => {
+
+// }, 1e8)
+
+const os = require("os");
+console.log(os);
+const platform = os.platform();
+const homeDirectory = os.homedir();
+console.log("platform", platform);
+console.log("homeDirectory", homeDirectory);
+
