288c8a5042dfc656ce31d4f2ab02a5833965d41f
Victoria Holub <victoria_holub@epam.com>
2023-04-19 17:30:09 +0300

    lesson 10 get, post, delete requests

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const Blog = require("./models/blog");
+const { render } = require("ejs");
 
 // express app
 const app = express();
@@ -27,6 +28,10 @@ app.set("views", path.join(__dirname, "front-end"));
 // log about request
 app.use(morgan("short"));
 
+// create middleware for encoding requests as POST
+// extended - розширений
+app.use(express.urlencoded({ extended: true }));
+
 // give access to that folder and all files inside
 app.use(express.static("public"));
 
@@ -49,15 +54,42 @@ app.get("/about", (req, res) => {
 
 // blogs routes
 app.get("/blogs", (req, res) => {
-  Blog.find().sort({ createdAt: -1 })
+  Blog.find()
+    .sort({ createdAt: -1 })
     .then((result) => {
       res.render("index", { title: "All Blogs", blogs: result });
     })
     .catch((err) => console.log(err));
 });
 
+app.post("/blogs", (req, res) => {
+  const blog = new Blog(req.body);
+  blog
+    .save()
+    .then((result) => res.redirect("/blogs"))
+    .catch((err) => console.log(err));
+});
+
 app.get("/blogs/create", (req, res) => {
   res.render("create");
+});
+
+app.get("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => {
+      res.render("details", { blog: result, title: "Blog Details" });
+    })
+    .catch((err) => console.log(err));
+});
+
+app.delete("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: "/blogs" });
+    })
+    .catch((err) => console.log(err));
 });
 
 // redirect
